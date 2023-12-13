7269099e59a865b61c8938fd26788c7d48690b33
12stephanegas12 <stephanegas11@gmail.com>
2023-12-13 10:52:56 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,47 +10,7 @@
 </head>
 
 
-<style>
-    body {
-        background-color: #fff;
-        color: #636b6f;
-        font-family: 'Nunito', sans-serif;
-        font-weight: 200;
-        height: 100vh;
-        margin: 0;
-    }
-</style>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Albert</a>
-</nav>
-DWAWDA
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div    id="app" class="s">
-                <note :note="note"></note>
-            </div>
-        </div>
-    </div>
-</div>
-
-<script src="app.js">
-
-
-
-</script>
-
-
-
-            <h1>Albert</h1>
-            <p>Albert is a simple web application that allows you to create, edit, delete and view your notes.</p>
-            <p>Albert is built with Laravel and Vue.js</p>
-            <p>Albert is open source and you can find the source code on <a href="">Github</a></p>
-        </div>
-    </div>
-</div>
-
 
 </body>
 </html>
